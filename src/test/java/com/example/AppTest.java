f6e8daec49a9549c78b918ac2b7127fe97fadefa
Jose Sanchez <jose.sanchez@portx.io>
2023-10-19 23:54:15 -0300

    Pwr 767 - Title to be presented in the release notes (#17)
    * PWR-767: first commit for it
    
    * PWR-767: second  commit for 767
    
    ---------
    
    Co-authored-by: jasf87 <5893218aB>

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -34,6 +34,6 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
-        System.out.println( "hello world!! sadsd !");
+        System.out.println( "hello world  !!     !");
     }
 }
